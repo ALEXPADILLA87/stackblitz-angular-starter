204b082357bedecc037e9e1e07a7c02f32d3f7fc
ALEXPADILLA87 <48296583+ALEXPADILLA87@users.noreply.github.com>
2023-09-22 16:44:31 -0500

    Initial commit

---
+++ src/app/components/array/array.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+@Component({
+  selector: 'app-array',
+  templateUrl: './array.component.html',
+  styleUrls: ['./array.component.css'],
+})
+export class ArrayComponent {
+  constructor() {}
+  setArray = new Set<number>();
+  displaySet: string = '';
+  array = [1, 2, 5];
+  array2 = [5, 6, 7];
+
+  set() {
+    this.setArray = new Set(this.array.concat(this.array2));
+    this.displaySet = Array.from(this.setArray).join(',');
+  }
+}
