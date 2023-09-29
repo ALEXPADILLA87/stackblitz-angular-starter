8a636a9204dd07e83fe525e739cfc8e1d36a035e
ALEXPADILLA87 <48296583+ALEXPADILLA87@users.noreply.github.com>
2023-09-29 12:05:25 -0500

    2 files modified

--- src/app/components/concepts/concepts.component.ts
+++ src/app/components/concepts/concepts.component.ts
@@ -6,13 +6,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConceptsComponent {
   constructor() {}
-  setArray = new Set<number>();
+  concatSet = new Set<number>();
+  spreadSet = new Set<number>();
   displaySet: string = '';
   array = [1, 2, 5];
   array2 = [5, 6, 7];
+  user: any;
+  copiedUser: any;
 
   set() {
-    this.setArray = new Set(this.array.concat(this.array2));
-    this.displaySet = Array.from(this.setArray).join(',');
+    this.concatSet = new Set(this.array.concat(this.array2));
+    this.spreadSet = new Set([...this.array, ...this.array2]);
+    this.displaySet = Array.from(this.spreadSet).join(',');
+  }
+
+  spread() {
+    this.user = { name: 'John Doe', age: 10 };
+    this.copiedUser = { ...this.user };
+    this.copiedUser.name = 'Clark Kent';
   }
 }
